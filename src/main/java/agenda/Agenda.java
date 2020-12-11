ae4112db7ab23fad67ac58e1501c3f78584aa201
Chinami <l.vong@hotmail.fr>
2020-12-11 21:11:06 +0100

    Add methods findByTitle and isFreeFor

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,20 +1,25 @@
 package agenda;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
  * Description : An agenda that stores events
  */
 public class Agenda {
+    
+    private List<Event> listEvents = new LinkedList<>();
+    
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     public void addEvent(Event e) {
+        this.listEvents.add(e);
         // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        //throw new UnsupportedOperationException("Pas encore implémenté");
     }
 
     /**
@@ -24,7 +29,43 @@ public class Agenda {
      * @return and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
+        List<Event> eventsDay = new LinkedList<>();
+        for (Event e : listEvents) {
+            if (e.isInDay(day)) {
+                eventsDay.add(e);
+            }
+        }
+        return eventsDay;
         // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        //throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+    
+     /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        List<Event> eventsInAgenda = new LinkedList<>();
+        for (Event e : listEvents) {
+            if (e.getTitle().equals(title)) {
+                eventsInAgenda.add(e);
+            }
+        }
+        return eventsInAgenda;
+    }
+    
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        for (Event event : listEvents) {
+            if (!e.getStart().equals(event.getStart()) && !e.getDuration().equals(event.getDuration())) {
+                return false;
+            }
+        }
+        return true;        
     }
 }
